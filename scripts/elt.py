4f7725d2cd5ace2db7f09d387b18e4c95e7cffb7
RealElvince <obuongely@yahoo.com>
2025-07-19 20:22:31 +0300

    read housing data using spark.read.parquet

--- scripts/elt.py
+++ scripts/elt.py
@@ -3,3 +3,10 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder \
     .appName("ELT Pipeline") \
     .getOrCreate()
+
+
+file_path = "data/ML_Data.parquet"
+
+airbnb_df = spark.read.parquet(file_path)
+
+airbnb_df.show(10)
