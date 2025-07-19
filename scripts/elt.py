5983e4f6298b5d76306fed72e29a3db8ed1067a7
RealElvince <obuongely@yahoo.com>
2025-07-19 20:32:41 +0300

    updated neighbourhood parameter

--- scripts/elt.py
+++ scripts/elt.py
@@ -9,4 +9,4 @@ file_path = "data/ML_Data.parquet"
 
 airbnb_df = spark.read.parquet(file_path)
 
-airbnb_df.select("neighborhood_cleansed", "room_type","bedrooms","bathrooms","number_of_reviews","price").show(10)
+airbnb_df.select("neighbourhood_cleansed", "room_type","bedrooms","bathrooms","number_of_reviews","price").show(10)
